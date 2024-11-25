13e8ee806c8333fcc8e7556f375ef05080bc6803
JD10976 <00001097667145sp@al.educacao.sp.gov.br>
2024-11-25 12:27:46 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geral',
+    ' Quantas cores tem o arco-íris? ',
+    'O arco-íris é composto por 7 cores refletidas no céu.'
+)
+
+criaCartao(
+    'Geral',
+    'Qual a montanha mais alta do mundo?',
+    'O Monte Everest'
+)
+
+criaCartao(
+    'Geral',
+    'Quem foi a primeira pessoa a viajar no espaço?',
+    'Yuri Gagarin'
+)
+
+criaCartao(
+    'Geral',
+    'Quais são as fases da Lua?',
+    'Nova, crescente, cheia e minguante. '
+)
